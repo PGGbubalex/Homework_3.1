4d0c9729eb8a7be88f4fdcdb16370b7b1f1f7274
Igor <99yearago99@gmail.com>
2022-09-21 19:23:43 +0300

    Homework 3

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+            int in = 222222;
+            System.out.println("Значение переменной in с типом int равно " + in);
+            byte by = 125;
+            System.out.println("Значение переменной by с типом byte равно " + by);
+            short sh = 12331;
+            System.out.println("Значение переменной sh с типом short равно " + sh);
+            long lo = 2232323232L;
+            System.out.println("Значение переменной lo с типом long равно " + lo);
+            float fl = 223.3333f;
+            System.out.println("Значение переменной fl с типом float равно " + fl);
+            double dou = 223.333333;
+            System.out.println("Значение переменной dou с типом double равно " + dou);
+
+        }
+    }
